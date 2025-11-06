86a8afdfe71fe83d68709b302bc814adb19a3fa5
Yoseph-GL <joseph88garza@gmail.com>
2025-11-06 09:40:38 -0600

    Add initial project structure and configuration files for RacingCars

---
+++ src/RacingCars/Cars.java
@@ -0,0 +1,12 @@
+/* PCACM
+Package: RacingCars
+Class: Cars
+Atribbutes
+Methods
+ */
+
+package RacingCars; //
+
+public class Cars { // Class declaration
+
+} // End of class Cars
---
+++ src/RacingCars/Main.java
@@ -0,0 +1,13 @@
+package RacingCars; // Package declaration
+
+public class Main { // Class declaration
+
+
+
+	public static void main(String[] args) { // Main method
+        System.out.print("TEST MAIN, DELETE THIS"); // Print statement
+	} // End of main method
+
+} // End of class main
+
+
