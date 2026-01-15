7e48f2776e12eaa8123ef046b78cdc4b8d5a1d7c
Joseph Angel Anthony Garza Lopez <joseph88garza@gmail.com>
2026-01-15 15:48:45 -0600

    Modify DRS boost in F1Car speed update
    Adjusted DRS boost value for speed calculation.

--- src/RacingCars/F1Car.java
+++ src/RacingCars/F1Car.java
@@ -3,15 +3,17 @@ package racingcars;
 public class F1Car extends Car {
 
     public F1Car(String driverName) {
-      
+
         super("Ferrari", "SF-24", "Red", 350, 0, driverName); 
     }
 
     @Override 
     public void updateSpeed() {
-        
-        double drsBoost = 1.8; 
         super.updateSpeed(); 
-  
+        
+        double drsBoost = 1.05; // 5% extra speed
+        this.currentSpeed = this.currentSpeed * drsBoost;
+
+        System.out.println("[F1 Telemetry] DRS Active. Speed adjusted.");
     }
 }
