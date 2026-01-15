15ab522bf2b534161fa6c89aab8d12f532d9e887
Joseph Angel Anthony Garza Lopez <joseph88garza@gmail.com>
2026-01-15 15:38:34 -0600

    Fix setModel method name to correct CamelCase

--- src/RacingCars/Cars.java
+++ src/RacingCars/Cars.java
@@ -88,13 +88,13 @@ public class Cars {
     }
 
     // --- Getters and Setters ---
-    // (Mantén tus getters y setters aquí, pero asegúrate de corregir 'setmodel' a 'setModel')
+   
 
     public String getBrand() { return brand; }
     public void setBrand(String brand) { this.brand = brand; }
 
     public String getModel() { return model; }
-    public void setModel(String model) { this.model = model; } // Corregido CamelCase
+    public void setModel(String model) { this.model = model; }
 
     public String getColor() { return color; }
     public void setColor(String color) { this.color = color; }
